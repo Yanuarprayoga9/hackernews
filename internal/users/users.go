07686633aa27b7029a81fc2ffa7238281355a399
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-10-29 13:36:39 +0700

    feat: auth middleware

--- internal/users/users.go
+++ internal/users/users.go
@@ -1,15 +1,16 @@
 package users
 
 import (
-	database "database/sql"
-	"golang.org/x/crypto/bcrypt"
-
+	"database/sql"
 	"log"
+
+	database "github.com/yanuar/hackernews/internal/pkg/db/migrations/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	ID       string `json:"id"`
-	Username     string `json:"name"`
+	Username string `json:"name"`
 	Password string `json:"password"`
 }
 
@@ -25,14 +26,32 @@ func (user *User) Create() {
 		log.Fatal(err)
 	}
 }
+//GetUserIdByUsername check if a user exists in database by given username
+func GetUserIdByUsername(username string) (int, error) {
+	statement, err := database.Db.Prepare("select ID from Users WHERE Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := statement.QueryRow(username)
 
-//HashPassword hashes given password
+	var Id int
+	err = row.Scan(&Id)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Print(err)
+		}
+		return 0, err
+	}
+
+	return Id, nil
+}
+// HashPassword hashes given password
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
-//CheckPassword hash compares raw password with it's hashed values
+// CheckPassword hash compares raw password with it's hashed values
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
